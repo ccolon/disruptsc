07553f4df45d4a00b1b5d2f3833ce1db93cbf0b0
ccolon <celian.colon@lemontri.fr>
2023-09-01 15:39:26 +0200

    keep admin_unit in firm_table

--- code/model/firm_builder_functions.py
+++ code/model/firm_builder_functions.py
@@ -163,7 +163,6 @@ def define_firms_from_local_economic_data(filepath_admin_unit_economic_data: Pat
     # C. Combine firms that are in the same od-point and in the same sector
     # group by od-point and sector
     firm_table_per_od_point = firm_table_per_admin_unit \
-        .drop(columns='admin_unit') \
         .groupby(['od_point', 'sector'], as_index=False) \
         .sum()
 
