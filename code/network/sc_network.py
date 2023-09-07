b3fc65fbd762a337f6c8703b9e25fdd7834b2bd4
ccolon <celian.colon@lemontri.fr>
2023-09-07 19:55:24 +0200

    import B2C flows

--- code/network/sc_network.py
+++ code/network/sc_network.py
@@ -14,13 +14,15 @@ class ScNetwork(nx.DiGraph):
                 add_or_append_to_dict(io, (supplier.sector, 'export'), commercial_link.order)
             elif commercial_link.category == "domestic_B2B":
                 add_or_append_to_dict(io, (supplier.sector, buyer.sector), commercial_link.order)
+            elif commercial_link.category == "import_B2C":
+                add_or_append_to_dict(io, ("IMP", 'final_demand'), commercial_link.order)
             elif commercial_link.category == "import":
                 add_or_append_to_dict(io, ("IMP", buyer.sector), commercial_link.order)
             elif commercial_link.category == "transit":
                 pass
             else:
                 raise KeyError('Commercial link categories should be one of domestic_B2B, '
-                               'domestic_B2C, export, import, transit')
+                               'domestic_B2C, export, import, import_B2C, transit')
 
         io_table = pd.Series(io).unstack().fillna(0)
         return io_table
