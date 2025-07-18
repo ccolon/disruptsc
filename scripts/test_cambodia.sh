ebb5f74c2bb05e2290fa77d480ebfdb0281b47c0
ElisaNobile <elisa.nobile@iusspavia.it>
2025-07-18 17:06:38 +0200

    updated analysis

--- scripts/test_cambodia.sh
+++ scripts/test_cambodia.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-durations=(1 3 7 14 21 28)
+durations=(1 3 7 14)
 simulation_names=(
-    "simultaneous_disruptions"
     "single_basin_saigon"
     "single_basin_mekong"
     "single_basin_tonle_sap"
-    "cascading_hazards_consecutive"
-    "cascading_hazards_time_lag_short"
-    "cascading_hazards_time_lag_long"
-    "cascading_and_compound_hazards_consecutive"
-    "cascading_and_compound_hazards_time_lag_short"
-    "cascading_and_compound_hazards_time_lag_long"
-    "cascading_and_compound_hazards_time_lag_very_long"
+    "cascading_hazards_0d"
+    "cascading_hazards_1d"
+    "cascading_hazards_2d"
+    "cascading_hazards_3d"
+    "cascading_hazards_4d"
+    "cascading_hazards_5d"
+    "cascading_hazards_6d"
+    "cascading_hazards_7d"
 )
 for duration in "${durations[@]}"; do
   for simulation_name in "${simulation_names[@]}"; do
-  job_name="flood_khm${duration}_${simulation_name}"
+  job_name="disr_khm${duration}_${simulation_name}"
   sbatch <<EOF
 #!/bin/bash
 #SBATCH --job-name=${job_name}
-#SBATCH --output=logs/Cambodia/${job_name}_%j.log
 #SBATCH --error=logs/Cambodia/${job_name}_%j.err
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
@@ -28,7 +27,7 @@ for duration in "${durations[@]}"; do
 echo "Job started: \$(date)"
 module purge
 source /projects/disruptsc/miniforge3/bin/activate /projects/disruptsc/miniforge3/envs/dsc
-python src/disruptsc/main.py Cambodia --duration ${duration} --simulation_name ${simulation_name}
+python src/disruptsc/main.py Cambodia --cache_isolation --duration ${duration} --simulation_name ${simulation_name}
 echo "Job ended: \$(date)"
 EOF
     sleep 5
